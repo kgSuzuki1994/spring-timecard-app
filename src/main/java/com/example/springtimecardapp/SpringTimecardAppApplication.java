644e47dd265c9145e18bfd7d30473a586e7ac273
kgSuzuki1994 <takuti0511@gmail.com>
2020-06-26 13:37:06 +0900

    init

---
+++ src/main/java/com/example/springtimecardapp/SpringTimecardAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtimecardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTimecardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTimecardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtimecardapp/SpringTimecardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtimecardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTimecardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
