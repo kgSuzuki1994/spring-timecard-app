888d608696ff91d442c8f5cab0bea279b1af896e
kgSuzuki1994 <takuti0511@gmail.com>
2020-07-01 22:58:24 +0900

    フォームに入力されたパスワードを暗号化するために利用する

--- src/main/java/com/example/springtimecardapp/config/WebSecurityConfig.java
+++ src/main/java/com/example/springtimecardapp/config/WebSecurityConfig.java
@@ -3,17 +3,26 @@ package com.example.springtimecardapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MessageSource messageSource;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
