e20d634f5e49ff52a13efb94785a307780d0105a
kgSuzuki1994 <takuti0511@gmail.com>
2020-07-02 00:53:26 +0900

    Config に認証関連の処理を追加

--- src/main/java/com/example/springtimecardapp/config/WebSecurityConfig.java
+++ src/main/java/com/example/springtimecardapp/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springtimecardapp.config;
 
+import com.example.springtimecardapp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
@@ -7,7 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,9 +22,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MessageSource messageSource;
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/images/**",
+                "/css/**",
+                "/javascript/**"
+        );
     }
 
     @Override
@@ -53,6 +68,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 }
             });
         }
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
 
