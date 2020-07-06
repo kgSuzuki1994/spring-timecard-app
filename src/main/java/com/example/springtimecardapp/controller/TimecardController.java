f1e03bcbf16bd2a7a4eb5fa1d885152cc5773291
kgSuzuki1994 <takuti0511@gmail.com>
2020-07-06 16:28:41 +0900

    /timecard でTimecardControllerクラスを参照するよう修正

--- src/main/java/com/example/springtimecardapp/controller/TimecardController.java
+++ src/main/java/com/example/springtimecardapp/controller/TimecardController.java
@@ -2,11 +2,13 @@ package com.example.springtimecardapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("timecard")
 public class TimecardController {
 
-    @GetMapping("timecard")
+    @GetMapping
     public String timecard() {
         return "timecard";
     }
