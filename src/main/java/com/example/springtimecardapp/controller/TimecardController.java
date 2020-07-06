ea4ffbd0bf4bf9799979a87cd33b814cc88e0d4e
kgSuzuki1994 <takuti0511@gmail.com>
2020-07-06 17:09:01 +0900

    出退勤のボタンを押したら /recorded に遷移するよう実装

--- src/main/java/com/example/springtimecardapp/controller/TimecardController.java
+++ src/main/java/com/example/springtimecardapp/controller/TimecardController.java
@@ -1,8 +1,11 @@
 package com.example.springtimecardapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("timecard")
@@ -11,5 +14,22 @@ public class TimecardController {
     @GetMapping
     public String timecard() {
         return "timecard";
+    }
+
+    @PostMapping("/recorded")
+    public String recorded(@RequestParam(value = "begin", required = false) String begin,
+                           @RequestParam(value = "finish", required = false) String finish,
+                           Model model) {
+        if (begin != null) {
+            model.addAttribute("showBeginMsg", true);
+            // TODO: DBに出社時刻を登録する
+            System.out.println("begin");
+        } else if (finish != null) {
+            model.addAttribute("showFinishMsg", true);
+            // TODO: DBに退社時刻を登録する
+            System.out.println("finish");
+        }
+
+        return "recorded";
     }
 }
