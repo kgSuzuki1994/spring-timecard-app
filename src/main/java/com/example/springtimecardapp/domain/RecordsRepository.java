97d5615b1a25bb9d11011c6f1b16bb44bb508ada
kgSuzuki1994 <takuti0511@gmail.com>
2020-07-08 21:20:55 +0900

    recordsテーブル用のリポジトリを作成

---
+++ src/main/java/com/example/springtimecardapp/domain/RecordsRepository.java
@@ -0,0 +1,6 @@
+package com.example.springtimecardapp.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordsRepository extends JpaRepository<Records, Long> {
+}
