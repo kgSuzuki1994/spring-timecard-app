259c3e0e42e05d87f47b4f4b33fe3d749e0eec8a
kgSuzuki1994 <takuti0511@gmail.com>
2020-07-09 16:24:31 +0900

    records テーブルの変更に合わせてエンティティの修正

--- src/main/java/com/example/springtimecardapp/domain/Records.java
+++ src/main/java/com/example/springtimecardapp/domain/Records.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -11,8 +13,12 @@ import javax.persistence.Id;
 @Setter
 public class Records {
 
-    /** ユーザID **/
+    /** 自動採番ID **/
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /** ユーザID **/
     private Long accountId;
 
     /** 出退勤した日付 **/
