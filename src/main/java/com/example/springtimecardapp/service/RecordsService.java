29095dfbb1b09ecc2a2c0b4cce0aa507d1ce35c2
kgSuzuki1994 <takuti0511@gmail.com>
2020-07-13 10:54:45 +0900

    退勤時間を設定する処理を実装

--- src/main/java/com/example/springtimecardapp/service/RecordsService.java
+++ src/main/java/com/example/springtimecardapp/service/RecordsService.java
@@ -1,5 +1,6 @@
 package com.example.springtimecardapp.service;
 
+import com.example.springtimecardapp.domain.Accounts;
 import com.example.springtimecardapp.domain.Records;
 import com.example.springtimecardapp.domain.RecordsRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 
 @Service
@@ -24,5 +24,28 @@ public class RecordsService {
         Long userId = accountsService.findUser(userName).getId();
 
         return recordsRepository.save(Records.newRecords(userId, beginTime));
+    }
+
+    public Records saveFinishTime(String userName, LocalDateTime finishTime) {
+
+        Long userId = accountsService.findUser(userName).getId();
+
+        Records records = selectLatestRecord(userId);
+
+        return recordsRepository.save(Records.setFinishTime(records, finishTime));
+    }
+
+    // ログインしているユーザIDで作成したレコードのうち、
+    // 一番最後に作成したレコード(最新レコード)を取得する
+    private Records selectLatestRecord(Long accountId) {
+
+        String query = "";
+        query += "SELECT * ";
+        query += "FROM records ";
+        query += "WHERE account_id = :accountId";
+        query += "ORDER BY begin_time DESC LIMIT 1";
+
+        return (Records) em.createNativeQuery(query, Records.class)
+                .setParameter("accountId", accountId).getSingleResult();
     }
 }
